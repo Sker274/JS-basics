a7d8b455a0f684064a3a619e7c77ca60f906a489
Sker274 <andrejlegat581@gmail.com>
2023-07-25 14:18:25 +0200

    adding project to github

---
+++ variables.js
@@ -0,0 +1,20 @@
+let a = 4
+console.log(a)
+
+console.log(typeof(a))
+
+let b = 2.346
+console.log(typeof(b))
+
+let c = "Skermaster"
+console.log(c)
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
+
+let count = a + b
+console.log(count)
+// We cannot redeclare variable with let keyword but possible with var
+
+console.log(!required)
