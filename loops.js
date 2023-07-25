0994b2c9cbfc22ea8ace799b0362957d6b33bd01
Sker274 <andrejlegat581@gmail.com>
2023-07-25 14:39:36 +0200

    adding loops.js

---
+++ loops.js
@@ -0,0 +1,20 @@
+const flag = true
+
+if(flag) {
+    console.log("Condition passed")
+}
+    else {
+        console.log("Condition not passed")
+    }
+
+
+let i = 0
+while(i < 10) {
+    i++
+    console.log(i)
+}
+
+do {
+    i++
+    console.log(i)
+}while(i > 11);
